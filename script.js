bf1921a7de3d600c89ac9d24bee0d249c3d05d22
Joshua861 <a8gd0ddf7hdh@gmail.com>
2023-05-08 20:56:02 +0100

    added cool new stuff

---
+++ script.js
@@ -0,0 +1,26 @@
+// Select the theme select element and the theme link
+const themeSelect = document.querySelector("#theme-select");
+const themeLink = document.querySelector("#theme-link");
+
+// Get the selected theme from local storage or default to "modern.css"
+const savedTheme = localStorage.getItem("selectedTheme") || "modern.css";
+
+// Set the initial theme select value and the theme link href
+themeSelect.value = savedTheme;
+themeLink.href = "css/" + savedTheme;
+
+// Log messages to confirm the initial values
+console.log("Selected theme:", savedTheme);
+console.log("Theme link href:", themeLink.href);
+
+// Add a change event listener to the theme select
+themeSelect.addEventListener("change", function() {
+  // Get the selected theme value and set the theme link href
+  const selectedTheme = themeSelect.value;
+  console.log("Selected theme:", selectedTheme);
+  themeLink.href = "css/" + selectedTheme;
+  console.log("Theme link href:", themeLink.href);
+
+  // Save the selected theme to local storage
+  localStorage.setItem("selectedTheme", selectedTheme);
+});

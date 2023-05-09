147eeb9b7bca58f785138e5e62a97cede5d68666
Joshua861 <a8gd0ddf7hdh@gmail.com>
2023-05-09 21:26:32 +0100

    added new themes and stopped it from being broken

--- theme-switcher.js
+++ theme-switcher.js
@@ -1,6 +1,7 @@
 // Select the theme select element and the theme link
 const themeSelect = document.querySelector("#theme-select");
 const themeLink = document.querySelector("#theme-link");
+const overlay = document.querySelector("#overlay");
 
 // Get the selected theme from local storage or default to "modern.css"
 const savedTheme = localStorage.getItem("selectedTheme") || "modern.css";
@@ -18,9 +19,15 @@ themeSelect.addEventListener("change", function() {
   // Get the selected theme value and set the theme link href
   const selectedTheme = themeSelect.value;
   console.log("Selected theme:", selectedTheme);
+  overlay.style.display = "block";
   themeLink.href = "css/" + selectedTheme;
   console.log("Theme link href:", themeLink.href);
 
   // Save the selected theme to local storage
   localStorage.setItem("selectedTheme", selectedTheme);
+  overlay.style.display = "none";
 });
+window.addEventListener("load", function() {
+    overlay.style.display = "none";
+  });
+  

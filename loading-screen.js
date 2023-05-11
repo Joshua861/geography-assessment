f99d34dfd586d0ede1ded7b6b54e47a25016f98c
Josh <a8gd0ddf7hdh@gmail.com>
2023-05-11 15:44:15 +0100

    Update loading-screen.js

--- loading-screen.js
+++ loading-screen.js
@@ -4,3 +4,16 @@ const overlay = document.querySelector('#load-bg')
 window.addEventListener('load', function () {
   overlay.style.display = 'none'
 })
+
+const loadingText = document.getElementById('loading-text');
+let dots = '';
+
+function updateLoadingText() {
+  loadingText.textContent = `Loading${dots}`;
+  dots += '..';
+  if (dots.length > 3) {
+    dots = '';
+  }
+}
+
+setInterval(updateLoadingText, 500); // Update every 500 milliseconds
